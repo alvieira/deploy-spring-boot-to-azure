a6a884921db57047c6ffd5ffc271d07a3ff7e640
Aloisio Vieira <aloisio.vieira@gmail.com>
2020-08-10 21:41:09 -0300

    deployed full stack app to azure

---
+++ 04-spring-boot-react-full-stack-h2/frontend/todo-app/build/precache-manifest.6f406935751bd30d596cf90fdde18488.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dea6e90f17d47bfa525",
+    "url": "/static/css/main.60c1a37b.chunk.css"
+  },
+  {
+    "revision": "5dea6e90f17d47bfa525",
+    "url": "/static/js/main.6604c92c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "4c4e45c21b9d5021aa11",
+    "url": "/static/js/2.aecb81c7.chunk.js"
+  },
+  {
+    "revision": "c7cdd8afd0bcd5accce067355d740500",
+    "url": "/index.html"
+  }
+];
--- 04-spring-boot-react-full-stack-h2/frontend/todo-app/build/service-worker.js
+++ 04-spring-boot-react-full-stack-h2/frontend/todo-app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.761a421da099d8debfc4c14fcfc2a75b.js"
+  "/precache-manifest.6f406935751bd30d596cf90fdde18488.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
